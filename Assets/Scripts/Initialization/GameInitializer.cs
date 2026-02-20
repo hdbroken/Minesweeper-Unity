87c8afec7cd9a2733b428d321912cbec0686fae4
MartinG <hdbroken@gmail.com>
2026-02-20 11:46:48 -0300

    Add GameInitializer prefab and script for board setup

---
+++ Assets/Scripts/Initialization/GameInitializer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameInitializer : MonoBehaviour
+{
+    [SerializeField] private BoardView _boardView;
+    private GameController _gameController;
+    
+    [SerializeField] private int _columns = 10;
+    [SerializeField] private int _rows = 10;
+    [SerializeField] private int _mines = 5;
+    [SerializeField] private int _proximityRange = 1;
+
+    private void Start()
+    {
+        Board board = new Board(_columns, _rows, _mines, _proximityRange);
+
+        _gameController = new GameController(board);
+
+        _boardView.Init(board, _gameController);        
+    }
+}
