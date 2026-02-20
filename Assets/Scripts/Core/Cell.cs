6d14c6e6d9816dbd7f187a7ef7d17a65e36f1071
MartinG <hdbroken@gmail.com>
2026-02-20 11:53:39 -0300

    Add states on cell (Empty, Flag, Question)

--- Assets/Scripts/Core/Cell.cs
+++ Assets/Scripts/Core/Cell.cs
@@ -1,19 +1,35 @@
+public enum CellMarkState
+{
+    Empty,
+    Flag,
+    Question
+}
+
 public class Cell
 {
     private bool _isMine;
     private bool _isRevealed;
-    private bool _isMarked;
+    private CellMarkState _markState = CellMarkState.Empty;
     private int _proximityCount;
 
     public bool IsMine => _isMine;
     public bool IsRevealed => _isRevealed;
-    public bool IsMarked => _isMarked;
+    public CellMarkState MarkState => _markState;
     public int ProximityCount => _proximityCount;
-
-    private const int MAX_NEIGHBORS = 8;
-
     public void SetMine(bool value) => _isMine = value;
     public void SetProximityCount(int count) => _proximityCount = count;
     public void Reveal() => _isRevealed = true;
-    public void ToggleMark() => _isMarked = !_isMarked;    
+
+    public void ToggleMark() 
+    { 
+        switch (_markState) 
+        { 
+            case CellMarkState.Empty: _markState = CellMarkState.Flag;
+                break; 
+            case CellMarkState.Flag: _markState = CellMarkState.Question;
+                break; 
+            case CellMarkState.Question: _markState = CellMarkState.Empty;
+                break;
+        } 
+    }
 }
