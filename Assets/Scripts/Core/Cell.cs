e614da9778690ad7e6dc99b902d9ab845e9a117f
MartinG <hdbroken@gmail.com>
2026-02-24 19:15:17 -0300

    Adding Interactable opcion to cell

--- Assets/Scripts/Core/Cell.cs
+++ Assets/Scripts/Core/Cell.cs
@@ -9,16 +9,19 @@ public class Cell
 {
     private bool _isMine;
     private bool _isRevealed;
+    private bool _isInteractable;
     private CellMarkState _markState = CellMarkState.Empty;
     private int _proximityCount;
 
     public bool IsMine => _isMine;
     public bool IsRevealed => _isRevealed;
+    public bool IsInteractable => _isInteractable;
     public CellMarkState MarkState => _markState;
     public int ProximityCount => _proximityCount;
     public void SetMine(bool value) => _isMine = value;
     public void SetProximityCount(int count) => _proximityCount = count;
     public void Reveal() => _isRevealed = true;
+    public void Interactable(bool isInteractable) => _isInteractable = isInteractable;
 
     public void ToggleMark() 
     { 
