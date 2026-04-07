5105fe1131e83f67191b582b07f171fcf3e1b2e0
MartinG <hdbroken@gmail.com>
2026-04-07 19:16:10 -0300

    Fix parameter name

--- Assets/Scripts/Interface/IBoardView.cs
+++ Assets/Scripts/Interface/IBoardView.cs
@@ -1,6 +1,6 @@
 public interface IBoardView
 {
-    void Init(BoardViewController boardViewControllerm, CameraController cameraController);
+    void Init(BoardViewController boardViewController, CameraController cameraController);
     void GenerateBoard(int columns, int rows);
     void ClearBoard();
     void UpdateCell(int column, int row);
